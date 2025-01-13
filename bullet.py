ba477de971e47e50f301f4c9d022184adbf69efd
Svetlana <bel-svetik@yandex.ru>
2025-01-14 02:36:33 +0300

    Добавлены звуки: фон, выстрел, враг

--- bullet.py
+++ bullet.py
@@ -2,7 +2,6 @@
 
 import pygame
 from pygame.math import Vector2
-# from main import TowerDefenseGame
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -18,22 +17,16 @@ class Bullet(pygame.sprite.Sprite):
         self.damage = damage
         self.velocity = self.calculate_velocity()
 
-        # Проиграть звук пули
-        self.game.shoot_sound.play()
-
     def calculate_velocity(self):
-        """ Вычисление скорости пули """
         direction = (self.target - self.position).normalize()
         velocity = direction * self.speed
         return velocity
 
     def update(self):
-        """ Обновление позиции пули """
         self.position += self.velocity
         self.rect.center = self.position
         if self.position.distance_to(self.target) < 10 or not self.game.is_position_inside(self.position):
             self.kill()
 
-    def is_position_inside(self, pos) -> bool:
-        """ Пуля в игровом поле """
+    def is_position_inside(self, pos):
         return 0 <= pos.x <= self.game.settings.screen_width and 0 <= pos.y <= self.game.settings.screen_height
